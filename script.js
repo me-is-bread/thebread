ef6e1a5c4b1656adff41a4d2d4628b94b6515804
Sahil Raut <neocrust98@gmail.com>
2023-11-21 14:20:06 +0530

    New files added

---
+++ script.js
@@ -0,0 +1,57 @@
+var audio_click = document.getElementById('click_player');
+var text = document.getElementById('title');
+var contentText = document.getElementById('content-text'); 
+
+var linkHolder = document.getElementById('link-holder');
+var linkBackText = document.getElementById('link-text-back');
+
+var contentShift = false;
+
+function play() {
+    var player = document.getElementById('player');
+    var status = document.getElementById('status');
+    if (player.muted == true) {
+        player.muted = false;
+        status.innerText = 'music_note'
+    }
+    else {
+        player.muted = true;
+        status.innerText = 'music_off'
+    }
+}
+
+body.onclick = function() {
+    audio_click.play();
+}
+
+function linkAbout () {
+    contentShift = true;
+    audio_click.play();
+    text.innerHTML = "About Me";
+    linkHolder.hidden = true;
+    linkBackText.hidden = false;
+}
+
+function linkCredits () {
+    contentShift = true;
+    audio_click.play();
+    text.innerHTML = "Credits";
+    contentText.innerHTML = 'Giga - Ready Steady ft. 初音ミク・鏡音リン・鏡音レン【MV】. <br/ > Video and song by Giga.';
+    linkHolder.hidden = true;
+    linkBackText.hidden = false;
+}
+
+function linkBack () {
+    if (contentShift == true) {
+        contentShift = false;
+        audio_click.play();
+        text.innerHTML = "Portfolio";
+        contentText.innerHTML = "Hi, my name is, <br /> <h1> Sahil Raut. <br /> I build things for Web. </h1> <br /> I'm a web developer specialized in UI/UX designs. <br /> (with designing software as Figma or Adobe XD and also with HTML, CSS, JS) <br /> I like to produce unique designs in every project.";
+        linkHolder.hidden = false;
+        linkBackText.hidden = true;
+    }
+
+    else {
+        return
+    }
+}
