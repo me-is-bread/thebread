17d8d0d8aec45fa1c222dca8a57ad71c600cc7f5
Sahil Raut <neocrust98@gmail.com>
2024-03-14 20:35:22 +0530

    Multiple file updation

--- projects/photography/script.js
+++ projects/photography/script.js
@@ -4,13 +4,13 @@ import { getStorage, ref, getDownloadURL } from "https://www.gstatic.com/firebas
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
 
 const firebaseConfig = {
-    apiKey: secrets.FIREBASE_APIKEY,
-    authDomain: secrets.FIREBASE_AUTHDOMAIN,
-    projectId: secrets.FIREBASE_PROJECT_ID,
-    storageBucket: secrets.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: secrets.FIREBASE_MESSAGING_SENDER_ID,
-    appId: secrets.FIREBASE_APP_ID,
-    measurementId: secrets.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
