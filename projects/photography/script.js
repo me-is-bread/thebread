3e46b3632c37721897b3cf0993db3d740a54e706
Sahil Raut <neocrust98@gmail.com>
2024-03-14 19:35:33 +0530

    updated files

--- projects/photography/script.js
+++ projects/photography/script.js
@@ -4,13 +4,13 @@ import { getStorage, ref, getDownloadURL } from "https://www.gstatic.com/firebas
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
 
 const firebaseConfig = {
-    apiKey: ${{ secrets.FIREBASE_APIKEY }},
-    authDomain: ${{ secrets.FIREBASE_AUTHDOMAIN }},
-    projectId: ${{ secrets.FIREBASE_PROJECT_ID }},
-    storageBucket: ${{ secrets.FIREBASE_STORAGE_BUCKET }},
-    messagingSenderId: ${{ secrets.FIREBASE_MESSAGING_SENDER_ID }},
-    appId: ${{ secrets.FIREBASE_APP_ID }},
-    measurementId: ${{ secrets.FIREBASE_MEASUREMENT_ID }}
+    apiKey: secrets.FIREBASE_APIKEY,
+    authDomain: secrets.FIREBASE_AUTHDOMAIN,
+    projectId: secrets.FIREBASE_PROJECT_ID,
+    storageBucket: secrets.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: secrets.FIREBASE_MESSAGING_SENDER_ID,
+    appId: secrets.FIREBASE_APP_ID,
+    measurementId: secrets.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
