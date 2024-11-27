717e5c717cad99e24a8fb024c373391496796ea1
Sahil Raut <mail.sahil.raut@gmail.com>
2024-11-27 14:25:33 +0530

    updated js and css

--- projects/renren/script.js
+++ projects/renren/script.js
@@ -34,7 +34,7 @@ function play() {
     var scene = new THREE.Scene();
     var group = new THREE.Group();
     var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0,0,100);
+    camera.position.set(0,0,150);
     camera.lookAt(scene.position);
     scene.add(camera);
 
